742538426b33c4ea2d47c259ed06ad4436de475a
Farabi9 <ifatkhan633@gmail.com>
2022-05-16 22:19:07 +0600

    booking backend connection

--- src/App.js
+++ src/App.js
@@ -11,19 +11,19 @@ import Navbar from "./Pages/Shared/Navbar";
 
 
 function App() {
-  
+
   return (
     <div className="max-w-7xl mx-auto">
-       <Navbar></Navbar>
-       <Routes>
-         <Route path="/" element={<Home></Home>}></Route>
-         <Route path="/about" element={<About></About>}></Route>
-         <Route path="/appoinment" element={<RequireAuth>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/appoinment" element={<RequireAuth>
           <Appoinment></Appoinment>
-         </RequireAuth>}></Route>
-         <Route path="/login" element={<Login></Login>}></Route>
-         <Route path="/signup" element={<SignUp></SignUp>}></Route>
-       </Routes>
+        </RequireAuth>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+      </Routes>
 
     </div>
   );
